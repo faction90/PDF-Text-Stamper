db20907228012e6b38d1dac2010bb9ab91dac122
Julien Tremblay <tremj90@hotmail.com>
2021-03-17 23:06:44 -0400

    1

---
+++ PDF Text Stamper/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace PDF_Text_Stamper
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStamp = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(13, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(457, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonStamp
+            // 
+            this.buttonStamp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStamp.Location = new System.Drawing.Point(395, 100);
+            this.buttonStamp.Name = "buttonStamp";
+            this.buttonStamp.Size = new System.Drawing.Size(75, 23);
+            this.buttonStamp.TabIndex = 1;
+            this.buttonStamp.Text = "Stamp!";
+            this.buttonStamp.UseVisualStyleBackColor = true;
+            this.buttonStamp.Click += new System.EventHandler(this.buttonStamp_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(13, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(457, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowse.Location = new System.Drawing.Point(394, 13);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrowse.TabIndex = 3;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(13, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(375, 23);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 135);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonStamp);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStamp;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ PDF Text Stamper/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows.Forms;
+using System;
+using System.IO;
+using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+
+//TODO : READ FROM EXCEL
+
+namespace PDF_Text_Stamper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Stamp text to PDF
+        //TODO : add parameter for font, size, position
+        public void StampPDF(PdfDocument pdfDoc, int j)
+        {
+            //Add text
+            PdfCanvas canvas = new PdfCanvas(pdfDoc.GetPage(j));
+            canvas.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(265, 597).ShowText(textBox1.Text).EndText();
+            //Add text
+            PdfCanvas canvas2 = new PdfCanvas(pdfDoc.GetPage(j));
+            canvas2.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(0, 0).ShowText(textBox2.Text).EndText();
+        }
+
+        //Get all file in the folder and stamp them
+        //TODO : validate path
+        //       Add proper sufix to file and/or create new directory
+        private void buttonStamp_Click(object sender, System.EventArgs e)
+        {
+            string[] fileEntries = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            foreach (string fileName in fileEntries)
+            {
+                PdfDocument pdfDoc = new PdfDocument(new PdfReader(fileName), new PdfWriter(fileName+"_M.pdf"));
+
+                int nbOfPages = pdfDoc.GetNumberOfPages();
+                for (int i = 1; i <= nbOfPages; i++)
+                {
+                    StampPDF(pdfDoc, i);
+                }
+                pdfDoc.Close();
+            }
+            MessageBox.Show("Fini!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //Select a folder
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = textBox3.Text;
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                textBox3.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
