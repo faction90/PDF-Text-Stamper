69411d32b73c6ddaee4a666bf46c26235d46c0a6
Julien Tremblay <tremj90@hotmail.com>
2021-03-18 19:38:48 -0400

    Avant implémentation Excel

--- PDF Text Stamper/Form1.Designer.cs
+++ PDF Text Stamper/Form1.Designer.cs
@@ -29,44 +29,26 @@ namespace PDF_Text_Stamper
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.buttonStamp = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.buttonBrowse = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.infoStampStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(13, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(457, 23);
-            this.textBox1.TabIndex = 0;
             // 
             // buttonStamp
             // 
             this.buttonStamp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonStamp.Enabled = false;
-            this.buttonStamp.Location = new System.Drawing.Point(395, 100);
+            this.buttonStamp.Location = new System.Drawing.Point(394, 42);
             this.buttonStamp.Name = "buttonStamp";
             this.buttonStamp.Size = new System.Drawing.Size(75, 23);
             this.buttonStamp.TabIndex = 1;
             this.buttonStamp.Text = "Stamp!";
             this.buttonStamp.UseVisualStyleBackColor = true;
             this.buttonStamp.Click += new System.EventHandler(this.buttonStamp_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(13, 71);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(457, 23);
-            this.textBox2.TabIndex = 2;
             // 
             // buttonBrowse
             // 
@@ -89,16 +71,35 @@ namespace PDF_Text_Stamper
             this.textBox3.Size = new System.Drawing.Size(375, 23);
             this.textBox3.TabIndex = 4;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Status :";
+            // 
+            // infoStampStatus
+            // 
+            this.infoStampStatus.AutoSize = true;
+            this.infoStampStatus.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.infoStampStatus.Location = new System.Drawing.Point(64, 44);
+            this.infoStampStatus.Name = "infoStampStatus";
+            this.infoStampStatus.Size = new System.Drawing.Size(57, 21);
+            this.infoStampStatus.TabIndex = 6;
+            this.infoStampStatus.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(481, 135);
+            this.ClientSize = new System.Drawing.Size(481, 76);
+            this.Controls.Add(this.infoStampStatus);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.buttonBrowse);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.buttonStamp);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -108,14 +109,13 @@ namespace PDF_Text_Stamper
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button buttonStamp;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button buttonBrowse;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label infoStampStatus;
     }
 }
 
--- PDF Text Stamper/Form1.cs
+++ PDF Text Stamper/Form1.cs
@@ -20,17 +20,17 @@ namespace PDF_Text_Stamper
 
         //Stamp text to PDF
         //TODO : add parameter for font, size, position
-        private void StampPDF(PdfDocument pdfDoc)
+        private void StampPDF(PdfDocument pdfDoc, string project, string mepBy, int qty, string color)
         {
             int nbOfPages = pdfDoc.GetNumberOfPages();
             for (int i = 1; i <= nbOfPages; i++)
             {
                 //Add text
                 PdfCanvas canvas = new PdfCanvas(pdfDoc.GetPage(i));
-                canvas.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(265, 597).ShowText(textBox1.Text).EndText();
-                //Add text
-                PdfCanvas canvas2 = new PdfCanvas(pdfDoc.GetPage(i));
-                canvas2.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(0, 0).ShowText(textBox2.Text).EndText();
+                canvas.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(0, 0).ShowText(project).EndText();
+                canvas.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(50, 50).ShowText(mepBy).EndText();
+                canvas.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(100, 100).ShowText(qty.ToString()).EndText();
+                canvas.BeginText().SetFontAndSize(PdfFontFactory.CreateFont(StandardFonts.HELVETICA), 12).MoveText(150, 150).ShowText(color).EndText();
             }
             pdfDoc.Close();
         }
@@ -40,39 +40,45 @@ namespace PDF_Text_Stamper
         //       Add proper sufix to file and/or create new directory
         private void buttonStamp_Click(object sender, System.EventArgs e)
         {
-            //---------------------------------------------------
-            //List<SpInfoBase> items = new()
-            //{
-            //    new SpInfoBase{ SpFileName="TEST", SpQuantity=5, SpColor="Jaune"},
-            //    new SpInfoBase{ SpFileName="TEST2", SpQuantity=1, SpColor="Bleu"}
-            //};
-
-            //MessageBox.Show(items[1].SpFileName);
-            //---------------------------------------------------
 
             if (Directory.Exists(folderBrowserDialog1.SelectedPath))
             {
                 string[] fileEntries = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+
+                //Load info from excel file DATA.xlsx
+
+                //---------------------------------------------------
+                //List<SpInfoBase> infoToStamp = new()
+                //{
+                //    new SpInfoBase{ SpFileName="TEST", SpQuantity=5, SpColor="Jaune"},
+                //    new SpInfoBase{ SpFileName="TEST2", SpQuantity=1, SpColor="Bleu"}
+                //};
+
+                //MessageBox.Show(infoToStamp[1].SpFileName);
+                //---------------------------------------------------
+
+                //Stamp all PDF in folder
                 foreach (string fileName in fileEntries)
                 {
                     if (Path.GetExtension(fileName).ToUpper() == ".PDF")
                     {
                         PdfDocument pdfDoc = new PdfDocument(new PdfReader(fileName), new PdfWriter(fileName + "_M.pdf"));
-                        StampPDF(pdfDoc);
+                        StampPDF(pdfDoc,"FT21-001","Julien Tremblay",4,"Ral 1023");
                     }
                 }
-                MessageBox.Show("Fini!");
+                infoStampStatus.Text = "Fini!";
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            infoStampStatus.Text = "";
         }
 
         //Select a folder
         private void buttonBrowse_Click(object sender, EventArgs e)
         {
+            infoStampStatus.Text = "";
             DialogResult result = folderBrowserDialog1.ShowDialog();
             if (result == DialogResult.OK) // Test result.
             {
